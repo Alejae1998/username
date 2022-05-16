79a9b0b9d60cf88cb73a3c5877c463b41c98af4b
Alejae1998 <alejagr1998@gmail.com>
2022-05-16 12:38:20 -0700

    sing up works and logout working

--- fetch-utils.js
+++ fetch-utils.js
@@ -11,16 +11,30 @@ export function getUser() {
 
 export async function signupUser(email, password) {
     const response = await client.auth.signUp({ email, password });
-    return response.user;
+    if (response.user) {
+        return response.user;
+    }
 }
 
 export async function signInUser(email, password) {
-    const response = await client.auth.sigIn({ email, password });
+    const response = await client.auth.signIn({ email, password });
     return response.user;
+
 }
 
-export async function checkAuth() {}
+export async function checkAuth() {
+    const user = getUser();
 
-export async function redirectIfLoggedIn() {}
+    if (!user) location.replace('/');
+}
 
-export async function logout() {}
+export async function redirectIfLoggedIn() {
+    if (getUser()) {
+        location.replace('./other-page');
+    }
+}
+
+export async function logout() {
+    await client.auth.signOut();
+    return (window.location.href = '/');
+}
--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,2 +1,11 @@
+import { checkAuth, logout } from '../fetch-utils.js';
+
+checkAuth();
+
+const logoutBtn = document.getElementById('logout');
+logoutBtn.addEventListener('click', async () => {
+    await logout();
+});
+
 // use checkAuth function to redirect is user not authenticated
 // add event listener to the logout button and call logout
